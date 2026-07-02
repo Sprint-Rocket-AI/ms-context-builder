48e2b7172fbb24e440b2b46342bb9426a2c98922
matiasvaldees <matias.valdes@bicevida.cl>
2026-07-02 12:38:15 -0400

    refactor(normalized-cors)

--- src/main/java/cl/sprint_rocket_ai/ms_context_builder/documents/infrastructure/config/CorsConfig.java
+++ src/main/java/cl/sprint_rocket_ai/ms_context_builder/documents/infrastructure/config/CorsConfig.java
@@ -11,18 +11,17 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
     @Value("${cors.allowed-origins}")
-    private String allowedOrigin;
+    private String allowedOrigins;
 
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config=new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList(allowedOrigin.split(",")));
+        config.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","PATCH"));
         config.setAllowedHeaders(Arrays.asList("Authorization","Content-Type"));
         config.setAllowCredentials(false);
